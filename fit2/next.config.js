bb27b26166b3acf68d6fa31433186149661a9d35
Monika Bugajewska <bugajewska.m@gmail.com>
2022-04-12 23:37:21 +0200

    Proxy requests to backend

--- fit2/next.config.js
+++ fit2/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/workouts/:path*',
+        destination: 'http://localhost:3000/workouts/:path*' // Proxy to Backend
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
